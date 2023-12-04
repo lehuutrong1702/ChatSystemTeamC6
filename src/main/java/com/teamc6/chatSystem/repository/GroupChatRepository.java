b450ba793a561203d37e1226559013a32139032f
nguyenpv.30@gmail.com <126704701+nguyen30042003@users.noreply.github.com>
2023-12-04 17:15:00 +0700

    Upload code group chat service and user

--- src/main/java/com/teamc6/chatSystem/repository/GroupChatRepository.java
+++ src/main/java/com/teamc6/chatSystem/repository/GroupChatRepository.java
@@ -2,6 +2,8 @@ package com.teamc6.chatSystem.repository;
 
 import com.teamc6.chatSystem.entity.GroupChat;
 import com.teamc6.chatSystem.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,13 +13,13 @@ import java.util.Optional;
 
 public interface GroupChatRepository extends JpaRepository<GroupChat,Long> {
     @Query("SELECT g FROM GroupChat g WHERE g.groupName LIKE %:searchName%")
-    List<GroupChat> filterName(@Param("searchName") String searchName);
+    Page<GroupChat> filterName(@Param("searchName") String searchName, Pageable pageable );
 
     @Query("SELECT g FROM GroupChat g ORDER BY g.groupName ASC")
-    List<GroupChat> sortOrderByNameAsc();
+    Page<GroupChat> sortOrderByNameAsc(Pageable pageable);
 
     @Query("SELECT g FROM GroupChat g ORDER BY g.timeCreate ASC")
-    List<GroupChat> sortOrderByCreateDateAsc();
+    Page<GroupChat> sortOrderByCreateDateAsc(Pageable pageable);
 
     @Query("SELECT g FROM GroupChat g WHERE g.groupName = :name")
     Optional<GroupChat> findByName(@Param("name") String username);
